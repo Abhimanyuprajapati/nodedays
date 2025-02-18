7887991c8b317524ba4dc9e39e6fd08ea5ac7c06
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-02-19 00:59:56 +0530

    event module

---
+++ day3/eventmodule2.js
@@ -0,0 +1,16 @@
+// 2. Event Module 
+
+// event module can create , fire and listen for our own events.and
+
+// An event is an action or an occurrence that has happened in our application that we can 
+// response to. Thinks of events like signals that trigger specific in your program.An
+
+// Using the event module we can dispatch our own custom events and response to those custom events 
+// in a non-blocking manner. 
+
+// why use Events?
+
+// 1. Asynchronous Programming : handle multiple task without blocking 
+// 2. Scalable Design : Easily add new event listeners without modifying existing code 
+
+
