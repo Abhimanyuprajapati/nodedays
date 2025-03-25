d889f932b417aae315c4518f6f32e73fd6048c5a
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-03-26 00:11:18 +0530

    working on node crud api

--- crud_api_project/index.js
+++ crud_api_project/index.js
@@ -17,46 +17,26 @@ app.get('/', (req, res) => {
   res.send('Hello World! to my l...');
 });
 
-app.post('/api/products',async (req, res) => {
+app.get('/api/products', async(req, res)=>{
+  try{
+    const product= await Product.find({});
+    res.status(200).json(product);
+  }catch(error){
+    res.status(500).json({message:error.message});
+  }
+})
 
+app.post('/api/products',async (req, res) => {
   try{
     const product= await Product.create(req.body);
     res.status(200).json(product);
   }catch(error){
     res.status(500).json({message: error.message})
   }
-  // console.log(req.body);
-  // res.send(req.body);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
-
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const app = express()
-// const port = 3000
-
-// app.use(express.json());  // this is the by default middleware 
-
-
-// mongoose.connect("mongodb+srv://proengineer18:proengineer@backendapi.uqyea.mongodb.net/?retryWrites=true&w=majority&appName=BackendAPI")
-//   .then(() => console.log('Connected to database!'));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World! to my l...')
-// })
-
-// app.post('/api/products', (req, res)=>{
-//   console.log(req.body);
-//   res.send(req.body);
-//   // res.send("data received");
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
 
