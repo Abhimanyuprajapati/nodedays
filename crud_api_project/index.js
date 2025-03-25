977d42b127372d38ff06a39f3117276f70671354
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-03-25 22:32:36 +0530

    using middleware in node js

--- crud_api_project/index.js
+++ crud_api_project/index.js
@@ -3,12 +3,20 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+app.use(express.json());  // this is the by default middleware 
+
 
 mongoose.connect("mongodb+srv://proengineer18:proengineer@backendapi.uqyea.mongodb.net/?retryWrites=true&w=majority&appName=BackendAPI")
   .then(() => console.log('Connected to database!'));
 
 app.get('/', (req, res) => {
   res.send('Hello World! to my l...')
+})
+
+app.post('/api/products', (req, res)=>{
+  console.log(req.body);
+  res.send(req.body);
+  // res.send("data received");
 })
 
 app.listen(port, () => {
