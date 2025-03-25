11d6d4da0817425e1e1c022c6d8b356cbd9fc570
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-03-25 22:39:57 +0530

    configure env file for security purpose

--- crud_api_project/index.js
+++ crud_api_project/index.js
@@ -1,30 +1,54 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const port = 3000
+require('dotenv').config(); // Load environment variables
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.use(express.json());  // this is the by default middleware 
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.json()); // Default middleware
 
-mongoose.connect("mongodb+srv://proengineer18:proengineer@backendapi.uqyea.mongodb.net/?retryWrites=true&w=majority&appName=BackendAPI")
-  .then(() => console.log('Connected to database!'));
+// Connect to MongoDB using the env variable
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to database!'))
+  .catch(err => console.error('Database connection error:', err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World! to my l...')
-})
+  res.send('Hello World! to my l...');
+});
 
-app.post('/api/products', (req, res)=>{
+app.post('/api/products', (req, res) => {
   console.log(req.body);
   res.send(req.body);
-  // res.send("data received");
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 
 
-// password :   CL2vxBQ1x2b77gJL
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const app = express()
+// const port = 3000
 
-// 
+// app.use(express.json());  // this is the by default middleware 
+
+
+// mongoose.connect("mongodb+srv://proengineer18:proengineer@backendapi.uqyea.mongodb.net/?retryWrites=true&w=majority&appName=BackendAPI")
+//   .then(() => console.log('Connected to database!'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! to my l...')
+// })
+
+// app.post('/api/products', (req, res)=>{
+//   console.log(req.body);
+//   res.send(req.body);
+//   // res.send("data received");
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
