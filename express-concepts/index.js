a5460e35d3858d532c9bbbdc2e2272c703bdaef8
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-04-05 15:18:42 +0530

    learning api versioning in node js

--- express-concepts/index.js
+++ express-concepts/index.js
@@ -4,6 +4,7 @@ const cors = require('cors'); // import cors module
 const corsConfig = require('./config/corsConfig'); // import cors configuration
 const { globalErrorHandler } = require('./middleware/errorHandler'); // import global error handler
 const { requestLogger, addTimeStamp } = require('./middleware/customMiddleware'); // import custom middleware functions
+const {urlVersioning} = require('./middleware/apiVersioning'); // import versioning middleware
 
 const app = express(); // create an instance of express
 const PORT = process.env.PORT || 1369; // set the port to listen on
@@ -15,6 +16,9 @@ app.use(addTimeStamp);
 app.use(express.json()); // parse json data in request body
 app.use(cors()); // enable cors for all routes
 app.use(corsConfig()); // enable cors with custom configuration
+
+
+app.use(urlVersioning('v1')); // use versioning middleware for v1
 
 app.use(globalErrorHandler); // use global error handler
 
