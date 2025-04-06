624467b76f21190cd72b4db810783ca54fc48d79
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-04-06 13:29:16 +0530

    learn how to connect to the redis in node js

--- redis/index.js
+++ redis/index.js
@@ -1,16 +1,23 @@
-const redis = require('redis');
+const redis = require("redis");
 const client = redis.createClient({
-    host: 'localhost',
-    port: 6379
-})
+  host: "localhost",
+  port: 6379,
+});
 
 // event listeners for redis client
-client.on("error", (err)=>{
-console.log("redis client error : ", err);
-})
+client.on("error", (err) => {
+  console.log("redis client error : ", err);
+});
 
+async function testConnectionRedis() {
+  try {
+    const res = await client.connect();
+    console.log("Redis client connected successfully", res);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.quit();
+  }
+}
 
-
-
-
-
+testConnectionRedis();
