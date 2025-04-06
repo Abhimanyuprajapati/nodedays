9865dec17e05f8410685ae0c673b2c2fa7b394b1
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-04-06 14:18:18 +0530

    basic operation on redis increment ,decrement , set, get and del methods

--- redis/index.js
+++ redis/index.js
@@ -12,7 +12,40 @@ client.on("error", (err) => {
 async function testConnectionRedis() {
   try {
     const res = await client.connect();
-    console.log("Redis client connected successfully", res);
+    // console.log("Redis client connected successfully", res);
+
+    const object = await client.set("manu", "value1");
+
+    console.log("Redis set response: ", object);
+
+    const extractValue = await client.get("manu");
+
+    console.log(extractValue);
+
+    const deleteValue = await client.del("manu")
+    console.log("Redis delete response: ", deleteValue);
+
+const extractedUpdatedValue = await client.get("manu");
+
+console.log(extractValue);
+    console.log(extractedUpdatedValue);
+
+await client.set("count", "100");
+const incrementValue = await client.incr("count");
+console.log(incrementValue);
+
+
+const decrementValue = await client.decr("count");
+await client.decr("count");
+await client.decr("count");
+await client.decr("count");
+await client.decr("count");
+await client.decr("count");
+
+const updatedDecrementValue = await client.get("count");
+console.log(updatedDecrementValue);
+
+
   } catch (err) {
     console.log(err);
   } finally {
