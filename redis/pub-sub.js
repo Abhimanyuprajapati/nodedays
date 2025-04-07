7e8ece9c496b6d95278dc60fe8b73a96b10e9897
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-04-07 20:41:42 +0530

    add pub-sub (subscriber) example using Redis in Node.js

---
+++ redis/pub-sub.js
@@ -0,0 +1,50 @@
+//  pub-sub
+// -> publiser -> send -> channel -> subscriber will consume
+
+const redis = require('redis');
+
+const client = redis.createClient({
+    host: "localhost",
+    port: 6379,
+})
+
+client.on('error', (err) => {
+    console.log('redis client error :', err)
+})
+
+async function testPubSubRedis () {
+    try{
+        await client.connect()
+        console.log('Redis client connected successfully')
+
+        const subscriber = client.duplicate(); // create a new client -> share the same connection
+        await subscriber.connect(); // connect to the redis server
+
+        await subscriber.subscribe("my-channel", (message, channel)=>{
+            console.log(`received message : ${channel} : ${message}`);
+        })
+
+        // publish message to the dummy channel
+        await client.publish("my-channel", "hello to redis world");
+
+        await client.publish("my-channel", "hello to my love");
+
+        await new Promise((resolve)=>{
+            setTimeout(() => {
+                resolve("done");
+            }, 5000);
+        })
+
+        await subscriber.unsubscribe("my-channel");
+        await subscriber.quit(); // close the subscriber connection
+
+    }catch(err){
+        console.log(err);
+    }finally{
+        client.quit();
+    }
+}
+
+
+testPubSubRedis();
+
