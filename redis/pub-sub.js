4e2483a72e219b1d89073a4ffc47fd6c1a5211ba
Abhimanyuprajapati <prajapatiabhimanyu18@gmail.com>
2025-04-07 23:49:58 +0530

    learning ioredis in node js

--- redis/pub-sub.js
+++ redis/pub-sub.js
@@ -17,26 +17,41 @@ async function testPubSubRedis () {
         await client.connect()
         console.log('Redis client connected successfully')
 
-        const subscriber = client.duplicate(); // create a new client -> share the same connection
-        await subscriber.connect(); // connect to the redis server
+        // const subscriber = client.duplicate(); // create a new client -> share the same connection
+        // await subscriber.connect(); // connect to the redis server
 
-        await subscriber.subscribe("my-channel", (message, channel)=>{
-            console.log(`received message : ${channel} : ${message}`);
-        })
+        // await subscriber.subscribe("my-channel", (message, channel)=>{
+        //     console.log(`received message : ${channel} : ${message}`);
+        // })
 
         // publish message to the dummy channel
-        await client.publish("my-channel", "hello to redis world");
+        // await client.publish("my-channel", "hello to redis world");
 
-        await client.publish("my-channel", "hello to my love");
+        // await client.publish("my-channel", "hello to my love");
 
-        await new Promise((resolve)=>{
-            setTimeout(() => {
-                resolve("done");
-            }, 5000);
-        })
+        // await new Promise((resolve)=>{
+        //     setTimeout(() => {
+        //         resolve("done");
+        //     }, 5000);
+        // })
 
-        await subscriber.unsubscribe("my-channel");
-        await subscriber.quit(); // close the subscriber connection
+        // await subscriber.unsubscribe("my-channel");
+        // await subscriber.quit(); // close the subscriber connection
+
+
+        // *** */ pipeline -> send multiple commands to redis server in a single request
+            // pipeline & transaction
+
+    const multi = client.multi();
+
+    multi.set("name:manu", "mylove");
+    multi.set("name:manu1", "missing");
+
+    multi.get("name:manu");
+    multi.get("name:manu1");
+
+    const result = await multi.exec();
+    console.log(result);
 
     }catch(err){
         console.log(err);
